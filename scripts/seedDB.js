f3f73bfa4ce0560b85d96da6172077c3f92cceae
Michael Myers <myersmyer@gmail.com>
2018-07-08 21:35:56 -0400

    more udpates

---
+++ scripts/seedDB.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const db = require('../models');
+mongoose.Promise = global.Promise;
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/nytreact',
+    {
+        useMongoClient: true
+    }
+);
+
