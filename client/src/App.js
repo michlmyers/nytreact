7e7ee7be15f022cf4b9666262e6fc9352e09661e
Michael Myers <myersmyer@gmail.com>
2018-07-09 22:09:24 -0400

    style changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Articles from "./pages/Articles";
 import Results from './pages/Results';
 import Saved from './pages/Saved';
-// import Nav from "./components/Nav";
 import Wrapper from './components/Wrapper';
-// import Articles from './pages/Articles';
 import './App.css';
 
 const App = () => (
   <Router>
   <div>
-    {/* <Nav /> */}
     <Wrapper>
       <Route exact path='/' component={Articles} />
       <Route path='/api/articles' component={Articles} />
--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -55,12 +55,15 @@ class Articles extends React.Component {
             <Container fluid>
                 <Row>
                     <Col size='md-10'>
+
                         <Jumbotron>
+                            <div class="shadow-lg p-3 mb-5 bg-white rounded">
                             <h1>
                             <i class="far fa-newspaper"></i> <br />
                             NY Times Article Search!</h1>
-                            <h2>Search a topic and save articles of interest</h2>
-                        </Jumbotron>
+                            </div> 
+                            <h3>Search a topic and save articles of interest</h3>
+                        </Jumbotron>   
                     </Col>
                 </Row>
                 <Row>
